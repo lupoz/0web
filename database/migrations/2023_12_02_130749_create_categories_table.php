dd7a2a50793ef217dfea06f15cab6f80c18f306a
lupoz <waltervilla82@gmail.com>
2023-12-02 14:28:36 +0100

    Migrations

--- database/migrations/2023_12_02_130749_create_categories_table.php
+++ database/migrations/2023_12_02_130749_create_categories_table.php
@@ -12,8 +12,26 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
+            // CATEGORY DATA
             $table->id();
+            $table->string('name', 30)->nullable()->unique();
+            $table->foreignId('parent_id')->nullable()->default(null)->constrained('categories')->nullOnDelete();
+
+            // SEO DATA
+            $table->string('slug', 50)->unique();
+            $table->string('title', 50)->nullable();
+            $table->string('meta_description', 160)->nullable();
+            $table->text('full_description')->nullable();
+
+            // ADVISE
+            $table->string('message_type', 10)->nullable();
+            $table->text('message_text')->nullable();
+
+            // OTHERS DATA
+            $table->boolean('active')->default(0);
+            $table->integer('hits')->default(1);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2023_12_02_130807_create_websites_table.php
+++ database/migrations/2023_12_02_130807_create_websites_table.php
@@ -12,8 +12,32 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('websites', function (Blueprint $table) {
+            // WEBSITE DATA
             $table->id();
+            $table->string('name', 30)->nullable();
+
+            // URLS
+            $table->string('display_url', 255)->unique();
+            $table->string('sponsored_url', 255)->unique()->nullable();
+
+            // SEO DATA
+            $table->string('slug', 50)->unique();
+            $table->string('title', 50)->nullable();
+            $table->string('meta_description', 160)->nullable();
+            $table->text('full_description')->nullable();
+
+            // OTHERS DATA
+            $table->boolean('active')->default(0);
+            $table->integer('hits')->default(1);
             $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('category_website', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('website_id')->constrained()->onDelete('cascade');
+
+            $table->unique(['category_id', 'website_id']);
         });
     }
 
